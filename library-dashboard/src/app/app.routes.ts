a7743e4241a7ecab773279f03de2fef011019130
Hell_Boy <vk1997vinodkumar@gmail.com>
2026-04-29 19:14:00 +0530

    wip

--- library-dashboard/src/app/app.routes.ts
+++ library-dashboard/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { PageNotFound } from './page-not-found/page-not-found';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: Login
+    },
+    {
+        path: 'register', component: Register
+    },
+    {
+        path: '**', component: PageNotFound
+    },
+    
+];
---
+++ library-dashboard/src/app/login/login.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatChipsModule
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+
+  loginForm: FormGroup;
+  hidePassword = true;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
+}
