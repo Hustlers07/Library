eab7b228d7a4f62f6f5484ede5877fee96a021e9
Hell_Boy <vk1997vinodkumar@gmail.com>
2026-04-30 22:46:46 +0530

    wip

--- user-management/src/main/java/com/library/user_management/config/SecurityConfig.java
+++ user-management/src/main/java/com/library/user_management/config/SecurityConfig.java
@@ -29,6 +29,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpMethod;
 
 /**
  * Spring Security Configuration
