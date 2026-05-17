b305f929a5cb0da13336281a31df0581995f8fc0
Hell_Boy <vk1997vinodkumar@gmail.com>
2026-05-17 23:18:48 +0530

    feat: profile page fetch and user service added

--- library-dashboard/src/app/components/profile/profile.ts
+++ library-dashboard/src/app/components/profile/profile.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UID } from '../../constants/api.constants';
+import { UserService } from '../../services/user-service';
+import { User } from '../../models/user/user-module';
 
 @Component({
   selector: 'app-profile',
@@ -11,12 +13,27 @@ import { UID } from '../../constants/api.constants';
 export class Profile implements OnInit {
 
   userId: string | null = null;
+  user: User | null = null;
+  errorMessage: string | null = null;
 
-  constructor(private router: ActivatedRoute){}
+  constructor(private router: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit(): void {
     this.userId = this.router.snapshot.paramMap.get(UID);
     console.log('User Id: ', this.userId);
+
+    if (this.userId) {
+      this.userService.getUserDetails(this.userId).subscribe({
+        next: user => {
+          console.log("User profile: ", user);
+          this.user = user;
+        },
+        error: err => {
+          this.errorMessage = err.message; // Show friendly message in UI
+        }
+      });
+    }
+
   }
 
 }
--- library-dashboard/src/app/services/user-service.ts
+++ library-dashboard/src/app/services/user-service.ts
@@ -1,8 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user/user-module';
+import { API_ENDPOINTS } from '../constants/api.constants';
+import { ConfigService } from './config-service';
+import { catchError, map, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  
+  constructor(private http: HttpClient, private config: ConfigService) { }
+
+  getUserDetails(uid: string) {
+    return this.http.get(API_ENDPOINTS.USERS(this.config) + "/" + uid).pipe(
+      map(profile => new User(profile)),
+      catchError(error => {
+        console.error('Error fetching user details:', error);
+        // Optionally transform the error into something user-friendly
+        return throwError(() => new Error('Failed to load user details'));
+      })
+    );
+  }
 }
