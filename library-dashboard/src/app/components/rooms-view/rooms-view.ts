e40f9af6a83e08cf64a6510f34c5de183583264a
Hell_Boy <vk1997vinodkumar@gmail.com>
2026-05-23 02:03:35 +0530

    feat: room view added

--- library-dashboard/src/app/components/rooms-view/rooms-view.ts
+++ library-dashboard/src/app/components/rooms-view/rooms-view.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {TitleCasePipe} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-rooms-view',
-  imports: [],
+  imports: [MatListModule, MatIconModule, TitleCasePipe],
   templateUrl: './rooms-view.html',
   styleUrl: './rooms-view.scss',
 })
-export class RoomsView {}
+export class RoomsView {
+onClick($event: PointerEvent) {
+throw new Error('Method not implemented.');
+}
+  fragments = ['inbox', 'outbox', 'drafts'];
+  activeLink: string = "no";
+}
