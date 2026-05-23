41474c490a6290de720e56d74c39f1a1930af673
Hell_Boy <vk1997vinodkumar@gmail.com>
2026-05-24 01:37:11 +0530

    fix: changes related to room form view

--- library-dashboard/src/app/components/room/room.ts
+++ library-dashboard/src/app/components/room/room.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { MatButtonToggleChange, MatButtonToggleModule } from '@angular/material/button-toggle';
+import { RoomService } from '../../services/room-service';
+import { FormControl } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-room',
-  imports: [],
+  imports: [MatButtonToggleModule],
   templateUrl: './room.html',
   styleUrl: './room.scss',
 })
-export class Room {}
+export class Room implements OnInit {
+
+
+
+  constructor(private roomService: RoomService) { }
+
+  actions = [
+    {
+      key: 'View',
+      value: 'view'
+    },
+    {
+      key: 'Create',
+      value: 'create'
+    },
+    {
+      key: 'Update',
+      value: 'update'
+    },
+    {
+      key: 'Delete',
+      value: 'delete'
+    }
+  ];
+  selectedChange: string=this.actions[0].value;
+
+  onActionChange(event: MatButtonToggleChange): void {
+    console.log(event.value);   // 'create' | 'update' | 'delete'
+    this.selectedChange = event.value;
+  }
+  ngOnInit(): void {
+
+  }
+
+
+}
