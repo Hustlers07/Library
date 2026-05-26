15e9fd4c369e8137d8446f142dfce8da5baf78b3
Groot <vk1997vinodkumar@gmail.com>
2026-05-27 02:36:30 +0530

    prod env properties updated

--- library-dashboard/src/environments/environment.prod.ts
+++ library-dashboard/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   devEmail: '',
   devPassword: '',
-  apiUrl: 'http://user-management-service:8080',
+  apiUrl: 'https://cotton-overripe-skyrocket.ngrok-free.dev',
   basePath: '/user-management'
 
 };
