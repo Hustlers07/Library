c7557f96059a946842ca00720c5e9ba07193d9ab
Groot <vk1997vinodkumar@gmail.com>
2026-05-27 22:28:58 +0530

    wip

--- library-dashboard/src/app/components/coupon/coupon.ts
+++ library-dashboard/src/app/components/coupon/coupon.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { MatButtonToggleChange, MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-coupon',
-  imports: [],
+  imports: [MatButtonToggleModule],
   templateUrl: './coupon.html',
   styleUrl: './coupon.scss',
 })
-export class Coupon {}
+export class Coupon {
+onActionChange($event: MatButtonToggleChange) {
+throw new Error('Method not implemented.');
+}
+    actions = [
+    {
+      key: 'View',
+      value: 'view'
+    },
+    {
+      key: 'Create',
+      value: 'create'
+    },
+    {
+      key: 'Update',
+      value: 'update'
+    }
+    
+  ];
+}
