c7d0071e342d3e255a0c47d0eea9e1c7742e14f6
Groot <vk1997vinodkumar@gmail.com>
2026-05-31 21:05:52 +0530

    wip

--- library-dashboard/src/environments/environment.ts
+++ library-dashboard/src/environments/environment.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: false,
   devEmail: 'dev@gmail.com',
   devPassword: 'devPassword',
-  // apiUrl: 'http://192.168.31.35:30003',
-  // basePath: 'user-management'
-
-  apiUrl: 'https://cotton-overripe-skyrocket.ngrok-free.dev',
+  apiUrl: 'http://192.168.31.35:30003',
   basePath: 'user-management'
+
+  // apiUrl: 'https://cotton-overripe-skyrocket.ngrok-free.dev',
+  // basePath: 'user-management'
 };
