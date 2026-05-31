bbc92e1c39877f1360d38be6f8219934586847db
Groot <vk1997vinodkumar@gmail.com>
2026-05-31 22:14:32 +0530

    wip

--- user-management/src/main/java/com/library/user_management/config/OpenAPIConfig.java
+++ user-management/src/main/java/com/library/user_management/config/OpenAPIConfig.java
@@ -3,9 +3,12 @@ package com.library.user_management.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.HeaderParameter;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
